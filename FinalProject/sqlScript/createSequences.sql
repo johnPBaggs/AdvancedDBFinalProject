471e39f85cfb96c36a59ef0323010bb68e2472c0
keerthiD0589 <kd49@students.uwf.edu>
2016-11-09 15:14:46 -0600

    Add files via upload
    adding sql procedures

---
+++ FinalProject/sqlScript/createSequences.sql
@@ -0,0 +1,25 @@
+CREATE SEQUENCE [dbo].[courseIds]  
+    AS int  
+    START WITH 1000  
+    INCREMENT BY 1  
+    ; 
+
+CREATE SEQUENCE [dbo].[questionIds]  
+    AS int  
+    START WITH 2000  
+    INCREMENT BY 1  
+    ;  
+
+CREATE SEQUENCE [dbo].[Ids]  
+    AS int  
+    START WITH 4000  
+    INCREMENT BY 1  
+    ;
+
+CREATE SEQUENCE [dbo].[testIds]  
+    AS int  
+    START WITH 10000  
+    INCREMENT BY 1  
+    ;
+
+
